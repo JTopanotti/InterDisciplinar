dc156a0f2e283d172dab16df05a23c89cb5f783f
Jonathan Topanotti <jontopanotti@gmail.com>
2017-05-24 16:38:16 -0300

    First Commit

---
+++ src/org/cc16/Tabela.java
@@ -0,0 +1,16 @@
+package org.cc16;
+
+/**
+ * Created by jonathan on 5/24/17.
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Tabela {
+    private String titulo;
+    private ArrayList<LinkedList<Integer>> linhas;
+    private ArrayList<String> cabecalho;
+    
+
+}
